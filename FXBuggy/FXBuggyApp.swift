04f54779cac34e65b4094e89c3de0561352d3cfa
hh404 <hjw231_2003@sina.com>
2025-08-18 10:46:12 +0800

    Initial Commit

---
+++ FXBuggy/FXBuggyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FXBuggyApp.swift
+//  FXBuggy
+//
+//  Created by hanshuang on 2025/8/18.
+//
+
+import SwiftUI
+
+@main
+struct FXBuggyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FXBuggyTests/FXBuggyTests.swift
@@ -0,0 +1,17 @@
+//
+//  FXBuggyTests.swift
+//  FXBuggyTests
+//
+//  Created by hanshuang on 2025/8/18.
+//
+
+import Testing
+@testable import FXBuggy
+
+struct FXBuggyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
